e59592df9193ca85f92d4b798e3f0c4c94837d84
DiogoMaMartins <mamartinsdiogo@gmail.com>
2019-03-05 21:25:18 +0100

    first application todo just basic

---
+++ First/vues.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id="app">
+    <Header/>
+    <AddTodo v-on:add-todo="addTodo"/>
+  <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Header from './components/layouts/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Todos,
+    AddTodo
+  },
+  data(){
+    return {
+      todos: [
+      /*{
+        id: 1,
+        title: "Todo one",
+        completed: false
+      },{
+        id: 2,
+        title: "Todo two",
+        completed:true
+      },{
+        id:3,
+        title: "Todo Three",
+        completed: false
+      }*/
+
+      ]      
+    }
+
+  },
+  methods: {
+    deleteTodo(id) {
+      axios.delete(`ttps://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+      //.catch(err => console.log(err))
+      //this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+
+      axios.post('https://jsonplaceholder.typicode.com/todos',{
+        title,
+        completed
+      })
+      .then( res => this.todos = [...this.todos, res.data])
+      //.catch(err => console.log(err))
+      //this.todos = [...this.todos, newTodo]
+    }
+  },
+  created() {
+     axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => this.todos = res.data)
+      //.catch(err => console.log(err));
+
+    }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin:0;
+  padding:0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+.btn {
+  display:inline-block;
+  border:none;
+  background:#555;
+  color:#fff;
+  padding: 7px 20px;
+  cursor:pointer;
+}
+
+.btn:hover {
+  background: #ccc;
+}
+</style>
